f4f8c500f43babdc8f34377099662599e58b0491
drunkdream <drunkdream@qq.com>
2020-10-28 07:59:20 +0800

    Update

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,5 @@
 import asyncio
 import sys
 
-if sys.platform == "win32":
+if sys.platform == "win32" and hasattr(asyncio, "WindowsSelectorEventLoopPolicy"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
